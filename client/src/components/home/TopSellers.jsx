686c7bc7d8cc8db1a47afe704a3cc81183cea36f
Mohamed Hussein <mohamedmhussein@protonmail.com>
2025-02-09 12:21:53 -0500

    Imports product data to topsellers section

---
+++ client/src/components/home/TopSellers.jsx
@@ -0,0 +1,14 @@
+'use client'
+import { useEffect, useState } from "react"
+import { IoBookSharp } from "react-icons/io5"
+import {books} from '../data/books'
+
+
+const TopSellers = () => {
+
+  return (
+    <div>TopSellers</div>
+  )
+}
+
+export default TopSellers
