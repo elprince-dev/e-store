5150f09a76607eaf78daff2bb0129177f071faa9
Mohamed Hussein <mohamedmhussein@protonmail.com>
2025-02-08 23:10:33 -0500

    Adds home component

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,10 @@
 import './page.css'
+import Home from "@/components/home/Home"
 
-export default function Home() {
+export default function page() {
   return (
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary" >
-        <h1>home</h1>
+        <Home />
       </main>
   );
 }
